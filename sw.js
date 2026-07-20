0249deead3e87f247c47a10d3df1a0eebda8f3c3
razberry1997 <razberry.pi97@gmail.com>
2026-07-20 12:11:24 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Service worker: cache-first per funzionare offline
+const CACHE = 'paperelle-v1';
+const FILES = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
